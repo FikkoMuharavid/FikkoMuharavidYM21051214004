57a1b122cf313589397f5299d8f9b3e95427c89f
FikkoMuharavid <114418487+FikkoMuharavid@users.noreply.github.com>
2022-11-22 21:48:47 +0700

    Add files via upload

---
+++ PBO-OperasiCRUD/index.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <title>Tugas PBO</title>
+  </head>
+  <body>
+        <nav class="navbar navbar-expand" style="background-color: #a7ffff;">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php">Tugas PBO</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link" href="T-Mhs.php">Tabel Mahasiswa</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="T-JK.php">Tabel Jenis Kelamin</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="T-Prodi.php">Tabel Prodi</a>
+                </li>
+            </ul>
+            </div>
+        </div>
+        </nav>
+
+      <div class="container"><br><br><br><br><br>
+        <dl class="row">
+          <dt class="col-sm-3">Nama</dt>
+          <dd class="col-sm-9">Fikko Muharavid Yoga Mardhany</dd>
+
+          <dt class="col-sm-3">Nim</dt>
+          <dd class="col-sm-9">21051214004</dd>
+
+          <dt class="col-sm-3">Kelas</dt>
+          <dd class="col-sm-9">S1 Sistem Informasi 2021 B</dd>
+        </dd>
+      </div>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+  </body>
+</html>
